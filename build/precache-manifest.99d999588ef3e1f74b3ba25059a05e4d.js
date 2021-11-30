132f0fc528823bcae4af14c290bf745dbefd2486
Saqib Attiq <saqibattiq7@gmail.com>
2021-11-30 16:14:06 +0500

    uplaod project on git and i am try for heroku hosting

---
+++ build/precache-manifest.99d999588ef3e1f74b3ba25059a05e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d99b66c0f3519dc26735617f7ac7ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8304c1861b9f2c4408e",
+    "url": "/static/css/2.5036aa7d.chunk.css"
+  },
+  {
+    "revision": "f5015071fc960b713688",
+    "url": "/static/css/main.dd27aa34.chunk.css"
+  },
+  {
+    "revision": "b8304c1861b9f2c4408e",
+    "url": "/static/js/2.de26c086.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de26c086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5015071fc960b713688",
+    "url": "/static/js/main.7721496c.chunk.js"
+  },
+  {
+    "revision": "6d4acce64fc192b12a3c",
+    "url": "/static/js/runtime-main.7c0c6312.js"
+  }
+]);
